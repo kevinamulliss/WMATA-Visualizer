d9656edc210d5fb2861e5059b0624ed6bc040ba6
kevinmulliss <kevinamulliss@gmail.com>
2025-01-21 12:04:15 -0500

    Initial Commit

---
+++ src/main/java/kevinamulliss/projects/wmata_visualizer/Main.java
@@ -0,0 +1,14 @@
+package kevinamulliss.projects.wmata_visualizer;
+
+/**
+ * Main class entrypoint for WMATA Visualizer.
+ */
+public class Main {
+    /**
+     * Main method program entrypoint.
+     * @param args Standard command line arguments for java program. Shoudl be left blank.
+     */
+    public static void main(String[] args) {
+       System.out.println("test output");
+    }
+}
